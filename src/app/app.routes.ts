314b55410cafdab0b3f5b63a73ff24a611e7a7a7
Dazi Dev <shadowapex308@gmail.com>
2026-02-11 11:24:06 +0100

    first commit for y ecommerce

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Admin } from './component/admin/admin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // {
+    //     path:'admin',
+    //     component: Admin
+    // }
+    {
+        path: "admin",
+        loadComponent:() => import('./component/admin/admin').then(c => c.Admin)
+    }
+];
